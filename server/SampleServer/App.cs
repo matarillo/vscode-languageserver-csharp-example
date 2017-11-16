c1fa4089595ed851abf1fc76a77c2937baa5bc7e
Kentaro Inomata <kentaro.inomata@gmail.com>
2017-11-16 11:28:17 -0800

    Copy from branch 'deprecated'

---
+++ server/SampleServer/App.cs
@@ -0,0 +1,149 @@
+﻿using LanguageServer;
+using LanguageServer.Client;
+using LanguageServer.Json;
+using LanguageServer.Parameters;
+using LanguageServer.Parameters.General;
+using LanguageServer.Parameters.TextDocument;
+using LanguageServer.Parameters.Workspace;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SampleServer
+{
+    public class App : ServiceConnection
+    {
+        private Uri _workerSpaceRoot;
+        private int _maxNumberOfProblems;
+        private TextDocumentManager _documents;
+
+        public App(Stream input, Stream output)
+            : base(input, output)
+        {
+            _documents = new TextDocumentManager();
+            _documents.Changed += Documents_Changed;
+        }
+
+        private void Documents_Changed(object sender, TextDocumentChangedEventArgs e)
+        {
+            ValidateTextDocument(e.Document);
+        }
+
+        protected override Result<InitializeResult, ResponseError<InitializeErrorData>> Initialize(InitializeParams @params)
+        {
+            _workerSpaceRoot = @params.rootUri;
+            var result = new InitializeResult
+            {
+                capabilities = new ServerCapabilities
+                {
+                    textDocumentSync = TextDocumentSyncKind.Full,
+                    completionProvider = new CompletionOptions
+                    {
+                        resolveProvider = true
+                    }
+                }
+            };
+            return Result<InitializeResult, ResponseError<InitializeErrorData>>.Success(result);
+        }
+
+        protected override void DidOpenTextDocument(DidOpenTextDocumentParams @params)
+        {
+            _documents.Add(@params.textDocument);
+            Logger.Instance.Log($"{@params.textDocument.uri} opened.");
+        }
+
+        protected override void DidChangeTextDocument(DidChangeTextDocumentParams @params)
+        {
+            _documents.Change(@params.textDocument.uri, @params.textDocument.version, @params.contentChanges);
+            Logger.Instance.Log($"{@params.textDocument.uri} changed.");
+        }
+
+        protected override void DidCloseTextDocument(DidCloseTextDocumentParams @params)
+        {
+            _documents.Remove(@params.textDocument.uri);
+            Logger.Instance.Log($"{@params.textDocument.uri} closed.");
+        }
+
+        protected override void DidChangeConfiguration(DidChangeConfigurationParams @params)
+        {
+            _maxNumberOfProblems = @params?.settings?.languageServerExample?.maxNumberOfProblems ?? 100;
+            foreach (var document in _documents.All)
+            {
+                ValidateTextDocument(document);
+            }
+        }
+
+        private void ValidateTextDocument(TextDocumentItem document)
+        {
+            var diagnostics = new List<Diagnostic>();
+            var lines = document.text.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+            var problems = 0;
+            for (var i = 0; i < lines.Length && problems < _maxNumberOfProblems; i++)
+            {
+                var line = lines[i];
+                var index = line.IndexOf("typescript");
+                if (index >= 0)
+                {
+                    problems++;
+                    diagnostics.Add(new Diagnostic
+                    {
+                        severity = DiagnosticSeverity.Warning,
+                        range = new Range
+                        {
+                            start = new Position { line = i, character = index },
+                            end = new Position { line = i, character = index + 10 }
+                        },
+                        message = $"{line.Substring(index, 10)} should be spelled TypeScript",
+                        source = "ex"
+                    });
+                }
+            }
+
+            Proxy.TextDocument.PublishDiagnostics(new PublishDiagnosticsParams
+            {
+                uri = document.uri,
+                diagnostics = diagnostics.ToArray()
+            });
+        }
+
+        protected override void DidChangeWatchedFiles(DidChangeWatchedFilesParams @params)
+        {
+            Logger.Instance.Log("We received an file change event");
+        }
+
+        protected override Result<ArrayOrObject<CompletionItem, CompletionList>, ResponseError> Completion(TextDocumentPositionParams @params)
+        {
+            var array = new[]
+            {
+                new CompletionItem
+                {
+                    label = "TypeScript",
+                    kind = CompletionItemKind.Text,
+                    data = 1
+                },
+                new CompletionItem
+                {
+                    label = "JavaScript",
+                    kind = CompletionItemKind.Text,
+                    data = 2
+                }
+            };
+            return Result<ArrayOrObject<CompletionItem, CompletionList>, ResponseError>.Success(array);
+        }
+
+        protected override Result<CompletionItem, ResponseError> ResolveCompletionItem(CompletionItem @params)
+        {
+            if (@params.data == 1)
+            {
+                @params.detail = "TypeScript details";
+                @params.documentation = "TypeScript documentation";
+            }
+            else if (@params.data == 2)
+            {
+                @params.detail = "JavaScript details";
+                @params.documentation = "JavaScript documentation";
+            }
+            return Result<CompletionItem, ResponseError>.Success(@params);
+        }
+    }
+}
